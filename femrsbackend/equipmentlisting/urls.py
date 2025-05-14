88217434b9c22ead8644a0c551233373edf3a245
Nageshwar <nageswararaonambari2001@gmail.com>
2025-05-14 10:35:29 +0530

    backend

--- femrsbackend/equipmentlisting/urls.py
+++ femrsbackend/equipmentlisting/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from femrsbackend.femrsbackend import settings
 from .views import EquipmentViewSet
 
 # Create a router and register our viewset
@@ -8,4 +10,4 @@ router.register(r'equipment', EquipmentViewSet, basename='equipment')
 
 urlpatterns = [
     path('', include(router.urls)),  # Includes all auto-generated routes
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
