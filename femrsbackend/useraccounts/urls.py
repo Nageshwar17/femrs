9826529627bae2d83c88b85525aa16d5a1a840c5
Nageshwar <nageswararaonambari2001@gmail.com>
2025-05-16 12:53:46 +0530

    feeling_good

--- femrsbackend/useraccounts/urls.py
+++ femrsbackend/useraccounts/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('create-admin/', create_admin, name='create_admin'),
 ]
