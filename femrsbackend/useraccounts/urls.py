5d021a2fd835142520533efd40948cd86b9ecd3e
Nageshwar <nageswararaonambari2001@gmail.com>
2025-05-16 14:31:04 +0530

    feelid

--- femrsbackend/useraccounts/urls.py
+++ femrsbackend/useraccounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, ProfileView, create_admin
+from .views import RegisterView, LoginView, LogoutView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
